0408e52238039a9737b006f43b75af916efe73e4
Ashik-2001 <ashikbabu2001@gmail.com>
2021-11-23 19:31:25 +0530

    WIP: Added fading appbar, set sample routes to tabs

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,65 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: ('Whatsapp Clone'),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'WhatsApp',
-            style: TextStyle(fontFamily: 'Helvetica Neue', color: Colors.grey),
-          ),
-          actions: <Widget>[
-            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
-          ],
-          backgroundColor: Colors.blueGrey[800],
-          elevation: 0,
-        ),
         body: DefaultTabController(
           length: 4,
-          child: Container(
-            child: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.camera_alt_rounded)),
-                Tab(text: 'Chats'),
-                Tab(text: 'Status'),
-                Tab(text: 'Calls'),
-              ],
+          child: Scaffold(
+            body: NestedScrollView(
+              headerSliverBuilder:
+                  (BuildContext context, bool innerBoxIsScrolled) {
+                return <Widget>[
+                  new SliverAppBar(
+                    title: Text(
+                      'WhatsApp',
+                      style: TextStyle(
+                          fontFamily: 'Helvetica Neue', color: Colors.grey),
+                    ),
+                    pinned: true,
+                    floating: true,
+                    actions: <Widget>[
+                      IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+                      IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+                    ],
+                    backgroundColor: Colors.blueGrey[900],
+                    bottom: TabBar(
+                      //isScrollable: true,
+                      tabs: [
+                        Tab(
+                          icon: Icon(Icons.camera_alt),
+                        ),
+                        Tab(child: Text('Chats')),
+                        Tab(child: Text('Status')),
+                        Tab(child: Text('Calls')),
+                      ],
+                    ),
+                  ),
+                ];
+              },
+              body: TabBarView(
+                children: <Widget>[
+                  camera_pages(),
+                  Icon(Icons.directions_transit, size: 350),
+                  Icon(Icons.directions_car, size: 350),
+                  Icon(Icons.directions_bike, size: 350),
+                ],
+              ),
             ),
-            color: Colors.blueGrey[800],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class camera_pages extends StatelessWidget {
+  const camera_pages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.black,
         ),
       ),
     );
